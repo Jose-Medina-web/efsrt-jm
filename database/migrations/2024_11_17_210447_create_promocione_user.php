1568fda0af985ae4ef4bf59e1f1e06733813dc5d
daparicio55 <daparicio@idexperujapon.edu.pe>
2024-11-21 12:51:57 -0500

    delete on cascade en promocione_user

--- database/migrations/2024_11_17_210447_create_promocione_user.php
+++ database/migrations/2024_11_17_210447_create_promocione_user.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('promocione_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('cascade');
             $table->foreign('promocione_id')->references('id')->on('promociones');            
         });
     }
